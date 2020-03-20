8c2100f9259c2230d8c9594840a8229728ba809e
Dave Liepmann <dave.liepmann@gmail.com>
2020-03-20 16:23:36 +0100

    Switch to meta-csv in China ns

--- src/appliedsciencestudio/covid19_clj_viz/china.clj
+++ src/appliedsciencestudio/covid19_clj_viz/china.clj
@@ -1,26 +1,23 @@
 (ns appliedsciencestudio.covid19-clj-viz.china
-  (:require [clojure.data.csv :as csv]
-            [clojure.java.io :as io]
-            [clojure.string :as string]
-            [jsonista.core :as json]))
+  (:require [clojure.string :as string]
+            [meta-csv.core :as mcsv]))
 
 (def cases
   "Current number of COVID19 cases in China, by province"
-  (->> (csv/read-csv (slurp "resources/COVID-19/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Confirmed.csv"))
-       rest
-       (map (juxt first second last))
-       (filter (comp #{"Mainland China"} second))
-       (reduce (fn [acc [province _ n]]
-                 (assoc acc province (Integer/parseInt n)))
+  (->> (mcsv/read-csv "resources/COVID-19/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Confirmed.csv"
+                      {:field-names-fn (comp keyword #(string/replace % #"/" "-") string/lower-case)})
+       (filter (comp #{"China" "Mainland China"} :country-region))
+       (reduce (fn [m {province :province-state n :3-5-20}]
+                 (assoc m province n))
                {})))
 
 (def province-populations
-  "from https://en.m.wikipedia.org/wiki/List_of_Chinese_administrative_divisions_by_population
+  "From https://en.m.wikipedia.org/wiki/List_of_Chinese_administrative_divisions_by_population#Current_population
   with some hand-correction"
-  (->> (csv/read-csv (slurp "resources/china.province-population.tsv")
-                     :separator \tab)
-       ;; only take the name of the province and 2017 population data
-       (mapv #(let [[province pop-s] (map string/trim %)]
+  (->> (mcsv/read-csv "resources/china.province-population.tsv")
+       ;; only take the name of the province and 2017 population data;
+       ;; rows after don't matter
+       (mapv #(let [[province pop-s] %]
                 [province (Integer/parseInt (string/replace pop-s "," ""))]))
        (into {})))
 
