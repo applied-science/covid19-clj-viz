1597ff888eac3084547e28a1eabf1fffa994a113
Yuliana Apaza <yuliana.apaza94@gmail.com>
2020-03-21 11:42:04 -0500

    ADD Peru geojson

--- src/appliedsciencestudio/covid19_clj_viz/south-america.clj
+++ src/appliedsciencestudio/covid19_clj_viz/south-america.clj
@@ -4,7 +4,16 @@
 (def cases
   "Current number of COVID19 cases in South America, by countries"
   (->> (csv/read-csv (slurp "resources/COVID-19/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Confirmed.csv"))
-       rest
+       (map (juxt first second last))
+       (filter (comp #{"Peru" "Brazil" "Argentina"
+                       "Chile" "Bolivia" "Colombia"
+                       "Ecuador" "Uruguay" "Paraguay" "Venezuela"
+                       "Suriname" "Guyana"} second))
+       ))
+
+(def peru-cases
+  "Current number of COVID19 cases in Peru by regions"
+  (->> (csv/read-csv (slurp "resources/deutschland.covid19cases.06-11-2020.tsv"))
        (map (juxt first second last))
        (filter (comp #{"Peru" "Brazil" "Argentina"
                        "Chile" "Bolivia" "Colombia"
