6946776e9fe9ee3cde6ee4135d30b2ec85770e94
Jack Rusher <jack@rusher.com>
2020-03-21 08:50:42 +0100

    meta-csv plus cleanup

--- src/appliedsciencestudio/covid19_clj_viz/deutschland.clj
+++ src/appliedsciencestudio/covid19_clj_viz/deutschland.clj
@@ -16,44 +16,55 @@
    "Schleswig Holstein" "Schleswig-Holstein"
    "Thuringia" "Thüringen"})
 
-(def cases
-  "Number of confirmed COVID-19 cases by German province (auf Deutsch).
-  Source: Robert Koch Institute https://www.rki.de/DE/Content/InfAZ/N/Neuartiges_Coronavirus/Fallzahlen.html"
-  (reduce (fn [acc m]
-            (assoc acc (get normalize-bundesland (:bundesland m) (:bundesland m))
-                   (Integer/parseInt (string/replace (:count m) "." ""))))
-          {}
-          (mcsv/read-csv "resources/deutschland.covid19cases.tsv"
-                         {:field-names-fn {"Bundesland" :bundesland
-                                           "Anzahl" :count
-                                           "Differenz zum Vortag" :difference-carried-forward
-                                           "Erkr./ 100.000 Einw." :sick-per-100k-residents
-                                           "Todesfälle" :deaths
-                                           "Besonders betroffene Gebiete in Deutschland" :particularly-affected-areas}
-                          :guess-types? false})))
-
 (def population
   "Population of German states.
   Source: Wikipedia https://en.m.wikipedia.org/wiki/List_of_German_states_by_population"
-  (->> (csv/read-csv (slurp (str "resources/deutschland.state-population.tsv"))
-                     :separator \tab)
-       rest ;; drop the column header line
-       (map (juxt first last)) ;; only take the name of the state and 2018 population data:
-       (map #(mapv string/trim %)) 
-       (map (fn [[state pop-s]]
-              [(get normalize-bundesland state state)
-               (Integer/parseInt (string/replace pop-s "," ""))]))
-       (into {})))
+  (->> (mcsv/read-csv "resources/deutschland.state-population.tsv"
+                      {:header? true
+                       :field-names-fn (comp keyword string/lower-case)})
+       (reduce (fn [m {state :state population :2018}]
+                 (assoc m
+                        (get normalize-bundesland state state)
+                        (Integer/parseInt (string/replace population "," ""))))
+               {})))
+
+(defn coerce-type-from-string
+  "Simplest possible type guessing, obviously not production-grade
+  heuristics."
+  [s]
+  (or (reduce (fn [_ convert]
+                (when-let [converted (try (convert s) (catch Exception e false))]
+                  (reduced converted)))
+              [#(or (nil? %) (empty? %)) nil
+               #(Integer/parseInt %)
+               #(Float/parseFloat %)])
+      s))
+
+(defn parse-german-number [s]
+  (if (string? s)
+    (-> (.replace s "+" "")
+        (.replace "." "")
+        (.replace "," ".")
+        coerce-type-from-string)
+    s))
 
 (def bundeslaender-data
-  "Map from bundesland to population, cases, and cases-per-100k persons."
-  (reduce (fn [acc [bundesland cases]]
-            (assoc acc
-                   bundesland
-                   {:state-or-province bundesland
-                    :population (population bundesland)
-                    :cases cases
-                    :cases-per-100k (double (/ cases (/ (population bundesland)
-                                                        100000)))}))
-          {}
-          (dissoc cases "Gesamt")))
+  "Number of confirmed COVID-19 cases by German province (auf Deutsch).
+  Source: Robert Koch Institute https://www.rki.de/DE/Content/InfAZ/N/Neuartiges_Coronavirus/Fallzahlen.html"
+  (->> (mcsv/read-csv "resources/deutschland.covid19cases.tsv"
+                      {:field-names-fn {"Bundesland" :bundesland
+                                        "Anzahl" :cases
+                                        "Differenz zum Vortag" :difference-carried-forward
+                                        "Erkr./ 100.000 Einw." :cases-per-100k
+                                        "Todesfälle" :deaths
+                                        "Besonders betroffene Gebiete in Deutschland" :particularly-affected-areas}
+                       :guess-types? false})
+       (mapv #(let [normed-bundesland (normalize-bundesland (:bundesland %) (:bundesland %))]
+                (vector normed-bundesland
+                        (-> (reduce (fn [m k] (update m k parse-german-number)) % (keys %))
+                            (assoc :population (population normed-bundesland))))))
+       (into {})))
+
+;; no longer needed because this information is also in the bundeslaender-data
+(def cases
+  (into {} (map (juxt :bundesland :cases) (vals bundeslaender-data))))
