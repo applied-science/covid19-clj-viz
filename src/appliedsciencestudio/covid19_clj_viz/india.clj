75996311e36d8dd85f69ba899b3c18f66539052d
Dave Liepmann <dave.liepmann@gmail.com>
2020-03-28 20:25:35 +0100

    Refactor India namespace
    * preprocess TSV to remove commas from numbers
    * remove unnecessary names in `let` bindings
    * simplify data pipelines; remove unnecessary `reduce`
    * add sorting and color-coding by country to bar charts
    * formatting
    * comment descriptions for all visualizations
    * namespace docstring
    * add some missing/broken `require`s
    * rename vars for clarity/simplicity

--- src/appliedsciencestudio/covid19_clj_viz/india.clj
+++ src/appliedsciencestudio/covid19_clj_viz/india.clj
@@ -1,29 +1,49 @@
 (ns appliedsciencestudio.covid19-clj-viz.india
-  (:require [jsonista.core :as json]
+  "Visualization of coronavirus situation in India.
+
+  Contributed by Noor Afshan Fathima."
+  (:require [appliedsciencestudio.covid19-clj-viz.common :refer [oz-config
+                                                                 applied-science-palette]]
+            [appliedsciencestudio.covid19-clj-viz.sources.johns-hopkins :as jh]
+            [clojure.set :refer [rename-keys]]
+            [clojure.string :as string]
+            [jsonista.core :as json]
             [meta-csv.core :as mcsv]
             [oz.core :as oz]))
 
-(defonce india-covid-data
+(comment
+  (oz/start-server! 8082)
+
+  )
+
+(defonce covid19india-json
   (slurp "https://api.covid19india.org/data.json"))
 
-(def state-population
-  "From https://en.wikipedia.org/wiki/List_of_states_and_union_territories_of_India_by_population"
-  (into {} (mcsv/read-csv "resources/india.state-population.tsv")))
+(def state-data
+  "Coronavirus cases, by Indian state"
+  (reduce (fn [m st]
+            (assoc m (:state st)
+                   {:confirmed (Integer/parseInt (:confirmed st))
+                    :active    (Integer/parseInt (:active st))
+                    :recovered (Integer/parseInt (:recovered st))
+                    :deaths    (Integer/parseInt (:deaths st))
+                    :state (:state st)}))
+          {}
+          (-> covid19india-json
+              (json/read-value (json/object-mapper {:decode-key-fn true}))
+              :statewise)))
 
-(def india-data
-  (->> (json/read-value india-covid-data (json/object-mapper {:decode-key-fn true}))
-       :statewise
-       (mapv (fn [each-state-data] (-> (:state each-state-data)
-                                      (hash-map each-state-data))))
-       (into {})))
+(def state-population
+  "From https://en.wikipedia.org/wiki/List_of_states_and_union_territories_of_India_by_population
+  with commas manually removed"
+  (into {} (map (juxt :State :Population)
+                (mcsv/read-csv "resources/india.state-population.tsv"))))
 
 
 ;;;; ===========================================================================
-
 ;; Minimum viable geographic visualization of India
 (oz/view! {:data {:url "/public/data/india-all-states.geo.json"
-                  :format {:type "json"
-                           :property "features"}}
+                  :format {:type "json" :property "features"}}
            :mark "geoshape"})
 
 (def india-dimensions
@@ -35,30 +55,31 @@
           :features
           (fn [features]
             (mapv (fn [feature]
-                    (let [cases (get-in india-data [(:NAME_1 (:properties feature)) :confirmed] 0)
-                          cases (if (not(= 0 cases))
-                                  (Long/parseLong cases)
-                                  cases)
-                          pop   (get state-population (:NAME_1 (:properties feature)))
-                          pop (if (not(= 0 pop))
-                                (Long/parseLong (string/replace pop "," ""))
-                                pop)
-                          cases-per-100k (if (< 0 cases)
-                                           (double (/ cases (/ pop 100000)))
-                                           0)]
+                    (let [state (:NAME_1 (:properties feature))
+                          cases (get-in state-data [state :confirmed] 0)]
                       (assoc feature
-                        :State          (:NAME_1 (:properties feature))
+                        :State          state
                         :Cases          cases
-                        :Deaths         (get-in india-data [(:NAME_1 (:properties feature)) :deaths] 0)
-                        :Recovered      (get-in india-data [(:NAME_1 (:properties feature)) :recovered] 0)
-                        :Cases-per-100k cases-per-100k)))
-
+                        :Deaths         (get-in state-data [state :deaths] 0)
+                        :Recovered      (get-in state-data [state :recovered] 0)
+                        :Cases-per-100k (double (/ cases
+                                                   (/ (get state-population state)
+                                                      100000))))))
                   features))))
 
 (comment
   (json/write-value (java.io.File. "resources/public/public/data/india-all-states-created.geo.json")
-                    india-geojson-with-data))
+                    india-geojson-with-data)
 
+  ;; for inspection without flooding my REPL, we ignore the many many coordinates:
+  (map #(dissoc % :geometry) (:features india-geojson-with-data))
+  
+  )
+
+
+;;;; ===========================================================================
+;;;; Choropleth of Coronavirus situation in India
+;;;; (It may take a while to load; I think because the geoJSON is very detailed)
 (oz/view!
   (merge-with merge oz-config india-dimensions
               {:title {:text "Current India COVID-19 Scenario"}
@@ -77,48 +98,41 @@
                                     {:field "Recovered" :type "quantitative"}]}
                :selection {:highlight {:on "mouseover" :type "single"}}}))
 
+
 ;;;; ===========================================================================
 ;;;; Bar chart with Indian states
+(oz/view!
+ (merge-with merge oz-config
+             {:title {:text "Confirmed COVID19 cases in India"}
+              :data {:values (->> state-data
+                                  vals
+                                  (map #(select-keys % [:state :confirmed]))
+                                  ;; ;; FIXME this is the line to toggle:
+                                  (remove (comp #{"Total"} :state)))},
+              :mark {:type "bar" :color (:green applied-science-palette)}
+              :encoding {:x {:field "confirmed", :type "quantitative"}
+                         :y {:field "state", :type "ordinal" :sort "-x"}}}))
 
-(oz/view!(merge oz-config
-                {:title "Confirmed COVID19 cases in India",
-                 :data {:values (->> india-data
-                                     vals
-                                     (map #(select-keys % [:state :confirmed]))
-                                     (reduce (fn [acc m]
-                                               (conj acc {:state (:state m)
-                                                          :confirmed (:confirmed m)}))
-                                             [])
-                                     (sort-by :state)
-                                     ;; ;; FIXME this is the line to toggle:
-                                     (remove (comp #{"Total"} :state)))},
-                 :mark {:type "bar" :color "#9085DA"}
-                 :encoding {:x {:field "confirmed", :type "quantitative"}
-                            :y {:field "state", :type "ordinal"
-                                :sort nil}}}))
 
+;;;; ===========================================================================
 ;;;; Bar chart with Indian states and Chinese provinces
-
 (oz/view!
   (merge oz-config
          {:title "Confirmed COVID19 cases in China and India",
           :data {:values (let [date "2020-03-19"]
                            (->> jh/confirmed
-                                ;; Notice improved readability from working with seq of maps:
                                 (map #(select-keys % [:province-state :country-region date]))
                                 (filter (comp #{"China" "Mainland China"} :country-region))
-                                (reduce (fn [acc m]
-                                          (conj acc {:state-province (:province-state m)
-                                                     :confirmed (get m date)}))
-                                        [])
-                                (concat (->> india-data
+                                (map #(rename-keys % {date :confirmed}))
+                                (concat (->> state-data
                                              vals
-                                             (map (comp #(select-keys % [:state-province :confirmed])
-                                                        #(rename-keys % {:state :state-province})))
+                                             (map (comp #(assoc % :country-region "India")
+                                                        #(rename-keys % {:state :province-state})))
                                              (sort-by :state)))
-                                (remove (comp #{"Hubei"} :state-province))
-                                (remove (comp #{"Total"} :state-province))))},
-          :mark {:type "bar" :color "#9085DA"}
+                                (remove (comp #{"Hubei" "Total"} :province-state))))}
+          :mark "bar"
           :encoding {:x {:field "confirmed", :type "quantitative"}
-                     :y {:field "state-province", :type "ordinal"
-                         :sort nil}}}))
+                     :y {:field "province-state", :type "ordinal" :sort "-x"}
+                     :color {:field "country-region" :type "ordinal"
+                             :scale {:range [(:purple applied-science-palette)
+                                             (:green applied-science-palette)]}}}}))
