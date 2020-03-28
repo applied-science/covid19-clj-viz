57b322914fb09a4325845ddf6233ab024fcd8c35
Noor Fathima <noorfathima@NoorFathima-MacBook-Pro.local>
2020-03-28 22:36:54 +0530

    Add India visualizations and fix indentations

--- src/appliedsciencestudio/covid19_clj_viz/explore.clj
+++ src/appliedsciencestudio/covid19_clj_viz/explore.clj
@@ -11,12 +11,14 @@
             [clojure.string :as string]
             [clojure.set]
             [jsonista.core :as json]
-            [oz.core :as oz]))
+            [oz.core :as oz]
+            [clojure.set :as set :refer [rename-keys]]
+            [appliedsciencestudio.covid19-clj-viz.india :as india]))
 
 (comment
-  (oz/start-server! 8082)
+  (oz/start-server! 8082))
 
-  )
+
 
 
 ;;;; ===========================================================================
@@ -141,8 +143,8 @@
                                     vals
                                     (into [])
                                     (map-indexed (fn [i n] {:cases n
-                                                           :country country
-                                                           :days-ago i}))))},
+                                                            :country country
+                                                            :days-ago i}))))},
               :mark {:type "bar" :size 24}
               :encoding {:x {:field "days-ago" :type "ordinal"
                              :sort "descending"}
@@ -184,9 +186,9 @@
                                                    :rate (jh/rate-as-of :confirmed cntry 1))))
                                         features))))))
 
-  (jh/new-daily-cases-in :deaths "Andorra")
+  (jh/new-daily-cases-in :deaths "Andorra"))
 
-  )
+
 
 (def europe-infection-datapoints
   (update europe-geojson :features
@@ -205,9 +207,9 @@
 
 (comment
   ;; Let's look at the rate of change data
-  (sort-by second (map (juxt :country :confirmed-rate) (:features europe-infection-datapoints)))
+  (sort-by second (map (juxt :country :confirmed-rate) (:features europe-infection-datapoints))))
 
-  )
+
 
 ;; The rate of infection is relatively constant across countries.
 (oz/view!
@@ -240,7 +242,7 @@
               :width 1200 :height 700
               :data {:values
                      (->> (map (fn [ctry] [ctry (zipmap jh/csv-dates (jh/country-totals ctry jh/confirmed))])
-                               (clojure.set/difference jh/countries #{"Mainland China" "China" "Others"} ))
+                               (clojure.set/difference jh/countries #{"Mainland China" "China" "Others"}))
                           (reduce (fn [vega-values [country date->cases]]
                                     (if (> 500 (apply max (vals date->cases))) ; ignore countries with fewer than X cases
                                       vega-values
@@ -258,3 +260,108 @@
                          :y {:field "cases", :type "quantitative"}
                          :color {:field "country", :type "nominal"}
                          :tooltip {:field "country", :type "nominal"}}}))
+
+;;;; ===========================================================================
+
+;; Minimum viable geographic visualization of India
+(oz/view! {:data {:url "/public/data/india-all-states.geo.json"
+                  :format {:type "json"
+                           :property "features"}}
+           :mark "geoshape"})
+
+(def india-dimensions
+  {:width 750 :height 750})
+
+(def india-geojson-with-data
+  (update (json/read-value (java.io.File. "resources/public/public/data/india-all-states.geo.json")
+                           (json/object-mapper {:decode-key-fn true}))
+          :features
+          (fn [features]
+            (mapv (fn [feature]
+                    (let [cases (get-in india/india-data [(:NAME_1 (:properties feature)) :confirmed] 0)
+                          cases (if (not(= 0 cases))
+                                  (Long/parseLong cases)
+                                  cases)
+                          pop   (get india/state-population (:NAME_1 (:properties feature)))
+                          pop (if (not(= 0 pop))
+                                (Long/parseLong (string/replace pop "," ""))
+                                pop)
+                          cases-per-100k (if (< 0 cases)
+                                           (double (/ cases (/ pop 100000)))
+                                           0)]
+                      (assoc feature
+                        :State          (:NAME_1 (:properties feature))
+                        :Cases          cases
+                        :Deaths         (get-in india/india-data [(:NAME_1 (:properties feature)) :deaths] 0)
+                        :Recovered      (get-in india/india-data [(:NAME_1 (:properties feature)) :recovered] 0)
+                        :Cases-per-100k cases-per-100k)))
+
+                  features))))
+
+(comment
+  (json/write-value (java.io.File. "resources/public/public/data/india-all-states-created.geo.json")
+                    india-geojson-with-data))
+
+(oz/view!
+  (merge-with merge oz-config india-dimensions
+              {:title {:text "Current India Covid Scenario"}
+               :data {:name "india"
+                      :values india-geojson-with-data
+                      :format {:property "features"}},
+               :mark {:type "geoshape" :stroke "white" :strokeWidth 1}
+               :encoding {:color {:field "Cases-per-100k",
+                                  :type "quantitative"
+                                  :scale {:field "cases-per-100k",
+                                          :scale {:type "log"}
+                                          :type "quantitative"}}
+                          :tooltip [{:field "State" :type "nominal"}
+                                    {:field "Cases" :type "quantitative"}
+                                    {:field "Deaths" :type "quantitative"}
+                                    {:field "Recovered" :type "quantitative"}]}
+               :selection {:highlight {:on "mouseover" :type "single"}}}))
+
+;;;; ===========================================================================
+;;;; Bar chart with Indian states
+
+(oz/view!(merge oz-config
+                {:title "Confirmed COVID19 cases in India",
+                 :data {:values (->> india/india-data
+                                     vals
+                                     (map #(select-keys % [:state :confirmed]))
+                                     (reduce (fn [acc m]
+                                               (conj acc {:state (:state m)
+                                                          :confirmed (:confirmed m)}))
+                                             [])
+                                     (sort-by :state)
+                                     ;; ;; FIXME this is the line to toggle:
+                                     (remove (comp #{"Total"} :state)))},
+                 :mark {:type "bar" :color "#9085DA"}
+                 :encoding {:x {:field "confirmed", :type "quantitative"}
+                            :y {:field "state", :type "ordinal"
+                                :sort nil}}}))
+
+;;;; Bar chart with Indian states and Chinese provinces
+
+(oz/view!
+  (merge oz-config
+         {:title "Confirmed COVID19 cases in China and India",
+          :data {:values (let [date "2020-03-19"]
+                           (->> jh/confirmed
+                                ;; Notice improved readability from working with seq of maps:
+                                (map #(select-keys % [:province-state :country-region date]))
+                                (filter (comp #{"China" "Mainland China"} :country-region))
+                                (reduce (fn [acc m]
+                                          (conj acc {:state-province (:province-state m)
+                                                     :confirmed (get m date)}))
+                                        [])
+                                (concat (->> india/india-data
+                                             vals
+                                             (map (comp #(select-keys % [:state-province :confirmed])
+                                                        #(rename-keys % {:state :state-province})))
+                                             (sort-by :state)))
+                                ;; ;; FIXME this is the line to toggle:
+                                (remove (comp #{"Hubei"} :state-province))))},
+          :mark {:type "bar" :color "#9085DA"}
+          :encoding {:x {:field "confirmed", :type "quantitative"}
+                     :y {:field "state-province", :type "ordinal"
+                         :sort nil}}}))
