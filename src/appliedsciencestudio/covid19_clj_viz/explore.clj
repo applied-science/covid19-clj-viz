8af045f13aa9f40a31f8b944148503d68a20594e
Dave Liepmann <dave.liepmann@gmail.com>
2020-03-30 12:20:28 +0200

    Formatting fixes
    (Whitespace changes were introduced in pull #3, possibly automatically
    by an IDE.)

--- src/appliedsciencestudio/covid19_clj_viz/explore.clj
+++ src/appliedsciencestudio/covid19_clj_viz/explore.clj
@@ -16,7 +16,9 @@
             [appliedsciencestudio.covid19-clj-viz.india :as india]))
 
 (comment
-  (oz/start-server! 8082))
+  (oz/start-server! 8082)
+
+  )
 
 
 
@@ -121,10 +123,10 @@
   (vals (take 10 (sort-by key #(compare %2 %1) (jh/new-daily-cases-in :confirmed "Germany"))))
   ;; (1477 1210 910 1597 170 451 281 136 241 129)
 
-  (vals (take 10 (sort-by key #(compare %2 %1) (jh/new-daily-cases-in :confirmed "Italy")))))
+  (vals (take 10 (sort-by key #(compare %2 %1) (jh/new-daily-cases-in :confirmed "Italy"))))
   ;; (3233 3590 3497 5198 0 2313 977 1797 1492 1247)
-  
 
+  )
 
 
 ;;;; ===========================================================================
@@ -143,8 +145,8 @@
                                     vals
                                     (into [])
                                     (map-indexed (fn [i n] {:cases n
-                                                            :country country
-                                                            :days-ago i}))))},
+                                                           :country country
+                                                           :days-ago i}))))},
               :mark {:type "bar" :size 24}
               :encoding {:x {:field "days-ago" :type "ordinal"
                              :sort "descending"}
@@ -187,7 +189,9 @@
                                                    :rate (jh/rate-as-of :confirmed cntry 1))))
                                         features))))))
 
-  (jh/new-daily-cases-in :deaths "Andorra"))
+  (jh/new-daily-cases-in :deaths "Andorra")
+
+  )
 
 
 
@@ -208,7 +212,9 @@
 
 (comment
   ;; Let's look at the rate of change data
-  (sort-by second (map (juxt :country :confirmed-rate) (:features europe-infection-datapoints))))
+  (sort-by second (map (juxt :country :confirmed-rate) (:features europe-infection-datapoints)))
+
+  )
 
 
 
